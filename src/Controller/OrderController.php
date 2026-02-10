26bf7b69e05f250088f1915afbff53f7e9c69fe1
Ivan Zavadsky <ivan.business.28@gmail.com>
2026-02-10 14:29:15 +0300

    Task 1: * Order CRUD created. * Views improved

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -1,28 +1,81 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Controller;
 
-use App\Application\Dto\CreateOrderDTO;
-use App\Application\Handler\CreateOrderHandler;
+use App\Entity\Order;
+use App\Form\OrderType;
+use App\Repository\OrderRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/order')]
 final class OrderController extends AbstractController
 {
-    #[Route('/api/v1/orders', methods: ['POST'])]
-    public function create(
-        Request $request,
-        CreateOrderHandler $handler
-    ): JsonResponse {
-//        $dto = CreateOrderDTO::fromRequest($request);
-//
-//        $orderId = $handler->handle($dto);
-        $orderId = 1;
+    #[Route(name: 'app_order_index', methods: ['GET'])]
+    public function index(OrderRepository $orderRepository): Response
+    {
+        return $this->render('order/index.html.twig', [
+            'orders' => $orderRepository->findAll(),
+        ]);
+    }
 
-        return $this->json(['id' => $orderId], 201);
-    }}
+    #[Route('/new', name: 'app_order_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $order = new Order();
+        $form = $this->createForm(OrderType::class, $order);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($order);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_order_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('order/new.html.twig', [
+            'order' => $order,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_order_show', methods: ['GET'])]
+    public function show(Order $order): Response
+    {
+        return $this->render('order/show.html.twig', [
+            'order' => $order,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_order_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Order $order, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(OrderType::class, $order);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_order_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('order/edit.html.twig', [
+            'order' => $order,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_order_delete', methods: ['POST'])]
+    public function delete(Request $request, Order $order, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$order->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($order);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_order_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
