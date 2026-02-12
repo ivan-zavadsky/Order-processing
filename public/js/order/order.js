a665880fe8e20ecf9e42de026a54767318559e48
Ivan Zavadsky <ivan.business.28@gmail.com>
2026-02-12 10:30:57 +0300

    Task 7: Improved js

--- public/js/order/order.js
+++ public/js/order/order.js
@@ -1,27 +1,24 @@
 import { add } from './add.js';
 import { getNext } from './add/next.js';
+import { rout } from './rout.js';
 
 let Orders = {
     listen: function () {
-        document.addEventListener('click', Orders.rout);
+        document.addEventListener(
+            'click',
+            (event) => Orders.rout(
+                event,
+                Orders.getNextItemNumber,
+                Orders.addOrderItem,
+                Orders.removeOrderItem,
+            )
+        );
     },
-    rout: function (event) {
-        const action = event.target.dataset.action;
-        switch (action) {
-            case 'order-items#add':
-                const maxNumber = Orders.getNextItemNumber();
-                Orders.addOrderItem(maxNumber);
-                break;
-            case 'order-items#remove':
-                Orders.removeOrderItem(event)
-        }
-
-    },
+    rout:              rout,
     getNextItemNumber: getNext,
     addOrderItem:      add,
     removeOrderItem: function (event) {
         event.target.closest('tr').remove();
-
     },
 }
 
---
+++ public/js/order/rout.js
@@ -0,0 +1,19 @@
+/**
+ * Роутер событий, который выполняет колбеки в зависимости от значения action в dataset
+ * @param {Event} event - объект события
+ * @param {Function} getNextNumber - функция для получения следующего номера
+ * @param {Function} addItem - функция для добавления элемента
+ * @param {Function} removeItem
+ */
+export function rout(event, getNextNumber, addItem, removeItem) {
+    const action = event.target.dataset.action;
+    switch (action) {
+        case 'order-items#add':
+            const maxNumber = getNextNumber();
+            addItem(maxNumber);
+            break;
+        case 'order-items#remove':
+            removeItem(event);
+    }
+}
+
